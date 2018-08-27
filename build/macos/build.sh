c412432979e0413ddb1d35a614740f00405bca57
Farshid Tavakolizadeh <email@farshid.ws>
2018-08-27 15:46:30 +0200

    update macOS-host buld scripts

--- build/macos/build.sh
+++ build/macos/build.sh
@@ -1,21 +1,26 @@
 #!/bin/sh
 
-# cleanup
+# cleanup old things
 docker rm agent-armv7
 docker rmi agent-armv7
 rm -fr temp
 
 set -e
-mkdir -p temp bin
-cp -R ../agent temp
-cp -R ../model temp
-cp -R ../vendor temp
 
+echo "Copying the code..."
+mkdir -p temp bin
+cp -R ../../agent temp
+cp -R ../../model temp
+cp -R ../../vendor temp
+
+echo "Compiling..."
 docker build -t agent-armv7 .
 docker run --name agent-armv7 agent-armv7
 docker cp agent-armv7:/home/bin/agent bin/agent-linux-armv7
 
-# cleanup
+echo "Cleaning up..."
 docker rm agent-armv7
 docker rmi agent-armv7
 rm -fr temp
+
+echo "Success!"
