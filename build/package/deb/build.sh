0acd072d42b7403459cd130b470414546c81be85
Farshid Tavakolizadeh <email@farshid.ws>
2018-08-29 17:00:43 +0200

    take executable name from arg

--- build/package/deb/build.sh
+++ build/package/deb/build.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
+if [ -z "$1" ]
+then
+      echo "name of executable not given as argument."
+      exit 1
+fi
+
 set -e
-#wget https://pipelines.linksmart.eu/browse/CPSW-DTB/latest/artifact/NB/Binary-Distributions/deployment-agent-linux-arm
+
+mv $1 deployment-agent
 
 mkdir -p linksmart-deployment-agent/DEBIAN
 mkdir -p linksmart-deployment-agent/lib/systemd/system
@@ -10,6 +17,6 @@ mkdir -p linksmart-deployment-agent/var/local/linksmart-deployment-agent
 
 cp control linksmart-deployment-agent/DEBIAN/
 cp linksmart-deployment-agent.service linksmart-deployment-agent/lib/systemd/system/
-mv deployment-agent-linux-arm linksmart-deployment-agent/usr/local/bin/
+mv deployment-agent linksmart-deployment-agent/usr/local/bin/
 
 dpkg-deb --build linksmart-deployment-agent
