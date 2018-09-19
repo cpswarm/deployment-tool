30afe059cc0a93f544f95977ab41126d5d08810c
Farshid Tavakolizadeh <email@farshid.ws>
2018-09-19 11:54:12 +0200

    added linux-amd64 build for agent

--- build/linux/build.sh
+++ build/linux/build.sh
@@ -9,8 +9,9 @@ echo "Copying the code..."
 package=code.linksmart.eu/dt/deployment-tool
 mkdir -p temp/$package bin
 cp -rv ../../manager temp/$package
+cp -rv ../../agent temp/$package
 cp -rv ../../model temp/$package
-cp -rv ../../vendor temp/$package
+cp -r ../../vendor temp/$package
 
 echo "Compiling... (IF HUNG, KILL THE CONTAINER!)"
 docker run --rm -v $(pwd)/temp:/home/src -v $(pwd)/bin:/home/bin -v $(pwd)/static-build.sh:/home/cmd.sh farshidtz/zeromq:golang-linux-amd64-stretch sh cmd.sh
