6ab2e576faa448ff00371c5b749b9a4eab80aec8
Farshid Tavakolizadeh <email@farshid.ws>
2020-01-21 18:07:11 +0100

    amd64 build: added flag to exit as soon as one fails

--- build/linux/static-build.sh
+++ build/linux/static-build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -e
+
 echo "BUILDING MANAGER"
 CGO_CPPFLAGS="-I/usr/include" CGO_LDFLAGS="-L/usr/lib -lzmq -lpthread -lsodium -lrt -lstdc++ -lm -lc -lgcc" \
 go build -mod=vendor -v --ldflags '-extldflags "-static"' -o bin/deployment-manager-linux-amd64 ./manager
